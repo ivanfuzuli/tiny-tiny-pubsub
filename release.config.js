931262e7fb49a56aa85882fb1722e04f26384e0b
Can Küçükyılmaz <can.kucukyilmaz@hotmail.com>
2022-02-27 21:45:33 +0300

    ci: fix typo

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm'],
-    ['@semantic-release/github', ,],
+    ['@semantic-release/github'],
     '@semantic-release/git',
   ],
   preset: 'angular',
