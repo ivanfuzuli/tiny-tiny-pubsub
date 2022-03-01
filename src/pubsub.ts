34698163f31e67bcbd009cc2fae5270ef7e8872c
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2022-03-02 00:40:18 +0300

    feat: wildcard support
    Closes #3

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -13,6 +13,31 @@ class PubSub implements PubSubInterface {
   private listeners: { [key: string]: Fn[] } = {};
 
   /**
+   *
+   * @param name - event name
+   * @param callback - callback will fired if its matches
+   * @returns {boolean}
+   */
+  private hasWildcard(name: string, callback: (key: string) => void): boolean {
+    const index = name.indexOf('*');
+
+    if (index > -1) {
+      const nameWithoutAsterix = name.slice(0, index);
+      const keys = Object.keys(this.listeners);
+
+      for (const key of keys) {
+        if (key.startsWith(nameWithoutAsterix)) {
+          callback(key);
+        }
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
    * it registers a new event with provided values
    *
    * @example
@@ -33,10 +58,21 @@ class PubSub implements PubSubInterface {
    * @example
    * pubsub.off('test', predefinedFn);
    *
+   * @example
+   * // with wildcard
+   * pubsub.off('test.*');
+   *
    * @param name {string} -  event name
    * @param fn  {function} callback function
+   * @returns {void}
    */
-  off(name: string, fn: Fn) {
+  off(name: string, fn?: Fn) {
+    const completed = this.hasWildcard(name, (key) => delete this.listeners[key]);
+
+    if (completed) {
+      return true;
+    }
+
     if (this.listeners[name]) {
       for (let i = 0; i < this.listeners[name].length; i++) {
         if (this.listeners[name][i] === fn) {
@@ -53,26 +89,43 @@ class PubSub implements PubSubInterface {
    * @example
    * * // without any data
    * pubsub.trigger("test");
+   * @example
    * // with string type data
    * pubsub.trigger("test", "nothing");
+   * @example
    * // with object type data
    * pubsub.trigger("test", {'hello', 'world'});
+   *
+   * @example
+   * // with wildcard
+   * pubsub.trigger("test.*", "data");
    *
    * @param name event name
    * @param data callback data
    * @returns {void}
    */
   trigger(name: string, data?: any) {
-    if (this.listeners[name]) {
-      this.listeners[name].forEach((fn: (data: any) => void) => {
+    const triggerAll = (key: string) => {
+      this.listeners[key].forEach((fn: (data: any) => void) => {
         fn(data);
       });
+    };
+    const completed = this.hasWildcard(name, (key) => {
+      triggerAll(key);
+    });
+
+    if (completed) {
+      return true;
+    }
+
+    if (this.listeners[name]) {
+      triggerAll(name);
     }
   }
 
   /**
    * clears all listener
-   * returns {void}
+   * @returns {void}
    */
   clear() {
     this.listeners = {};
--- test/pubsub.test.ts
+++ test/pubsub.test.ts
@@ -21,12 +21,38 @@ describe('pubSub', () => {
     expect(pubSub._listeners()).toEqual({ test: [] });
   });
 
+  test('event remove successfully with wildcard', () => {
+    const fn = jest.fn();
+
+    pubSub.on('test', fn);
+    pubSub.on('test.driven', fn);
+    pubSub.on('test.driven.development', fn);
+
+    pubSub.off('test.*');
+    expect(pubSub._listeners()).toEqual({ test: [fn] });
+  });
+
   test('event should be triggered', () => {
     const fn = jest.fn();
 
     pubSub.on('test', fn);
     pubSub.trigger('test');
     expect(fn).toHaveBeenCalled();
+  });
+
+  test('event should be triggered with wildcard', () => {
+    const fn = jest.fn();
+    const fn2 = jest.fn();
+    const fn3 = jest.fn();
+
+    pubSub.on('test', fn);
+    pubSub.on('test.driven', fn2);
+    pubSub.on('test.driven.development', fn3);
+
+    pubSub.trigger('test*');
+    expect(fn).toHaveBeenCalled();
+    expect(fn2).toHaveBeenCalled();
+    expect(fn3).toHaveBeenCalled();
   });
 
   test('event should be triggered with data', () => {
@@ -39,7 +65,6 @@ describe('pubSub', () => {
 
   test('clear all listeners', () => {
     const fn = jest.fn();
-    const fn2 = jest.fn();
 
     pubSub.on('test', fn);
     pubSub.clear();
